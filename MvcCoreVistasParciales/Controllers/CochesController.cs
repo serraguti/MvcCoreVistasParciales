2761b6a72537280753bf45b16b287bc4fed799d7
serraguti <pacoserranox@gmail.com>
2022-01-31 10:04:56 +0100

    Add project files.

---
+++ MvcCoreVistasParciales/Controllers/CochesController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreVistasParciales.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreVistasParciales.Controllers
+{
+    public class CochesController : Controller
+    {
+        private List<Coche> Cars;
+
+        public CochesController()
+        {
+            this.Cars = new List<Coche>
+            {
+                new Coche{ IdCoche = 1, Marca = "Pontiac", Modelo = "FireBird"
+                , VelocidadMaxima = 320 },
+                new Coche { IdCoche = 2, Marca = "Lamborghini", Modelo = "Diablo"
+                , VelocidadMaxima = 340},
+                new Coche { IdCoche = 3, Marca = "Ferrari", Modelo= "Testarrossa"
+                , VelocidadMaxima = 300},
+                new Coche { IdCoche = 4, Marca = "Fiat", Modelo = "Panda"
+                , VelocidadMaxima = 95},
+                new Coche { IdCoche = 5, Marca = "Ford", Modelo = "Mustang GT"
+                , VelocidadMaxima = 290}
+            };
+        }
+
+        public IActionResult Index()
+        {
+            return View(this.Cars);
+        }
+
+        //ESTA VISTA NO TENDRA NADA AL CARGAR
+        //EN SU INTERIOR, CARGAREMOS VISTAS PARCIALES
+        //CON JQUERY
+        //TENDREMOS UNA VISTA PARCIAL CON TODOS LOS COCHES
+        public IActionResult PeticionAsincrona()
+        {
+            return View();
+        }
+
+        //NECESITAMOS UN METODO QUE SERA LLAMADO MEDIANTE 
+        //load() DE JQUERY
+        //LOS METODOS IACTIONRESULT SIEMPRE DEVUELVEN 
+        //PartialView
+        public IActionResult _CochesPartial()
+        {
+            return PartialView("_CochesPartial", this.Cars);
+        }
+    }
+}
